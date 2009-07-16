f2d97013dbf3d69d3827ef65aca9314ba4440dc1
jugyo <jugyo.org@gmail.com>
2009-07-16 15:18:32 +0900

    store to DB

--- examples/source_analyzer.rb
+++ examples/source_analyzer.rb
@@ -1,9 +1,29 @@
 #!/usr/bin/env ruby
 
+require 'rubygems'
+require 'sequel'
 $: << File.dirname(__FILE__) + '/../lib'
 require 'streamtter'
 
+DB = Sequel.sqlite("#{Dir.pwd}/source_analyzer.db")
+unless DB.table_exists?(:logs)
+  DB.create_table :logs do
+    [:status_id, :app_name, :app_url, :user_id, :user_name].each do |name|
+      String name
+    end
+  end
+end
+
 username, password = *ARGV[0..1]
 Streamtter.start(username, password) do |status|
-  puts status["source"]
+  if /<a href="(.*)?">(.*)?<\/a>/ =~ status['source']
+    app_name, app_url = $1, $2
+    DB[:logs] << {
+      :status_id => status['id'],
+      :app_name => app_name,
+      :app_url => app_url,
+      :user_id => status['user']['id'],
+      :user_name => status['user']['screen_name']
+    }
+  end
 end
