2eef0c7e878194fdc5756275279e0091e985dbcd
jugyo <jugyo.org@gmail.com>
2009-07-16 11:36:14 +0900

    Implemented a Streamtter class.

---
+++ lib/streamtter.rb
@@ -0,0 +1,32 @@
+require 'rubygems'
+require "socket"
+require 'base64'
+require 'json'
+
+class Streamtter
+  attr_accessor :username, :password
+
+  def initialize(username, password)
+    @username = username
+    @password = password
+  end
+
+  def start(&block)
+    s = TCPSocket.open("stream.twitter.com", 80)
+    auth = Base64.b64encode("#{username}:#{password}")
+    s.write <<-EOS
+GET /spritzer.json HTTP/1.1
+Host: stream.twitter.com
+Authorization: Basic #{auth}
+    EOS
+
+    begin
+      while str = s.gets
+        json = JSON.parse(str) rescue nil
+        block.call(json) if json
+      end
+    ensure
+      s.close
+    end
+  end
+end
